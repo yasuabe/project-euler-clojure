9108147c76b7aac3856ad6fd3edaad867411475e
Yasuyuki Abe <yasu@yasuabe.net>
2020-02-01 04:18:21 +0900

    solve 001

--- src/pe/001.clj
+++ src/pe/001.clj
@@ -1,28 +1,27 @@
 (ns pe.001)
 
-(defn mult-of-3or5?
+; ------ using brute force
+(defn divisible?
   [n]
   (let [f (fn [d] (= 0 (rem n d)))]
     (or (f 5) (f 3)))) 
 
 (defn solve
-  "using brute force"
   [n]
-  (reduce + (filter mult-of-3or5? (range 1 n))))
+  (reduce + (filter divisible? (range 1 n))))
 
-(solve 1000)
 
+; ------ using math
 (defn mult-sum
   [n d]
   (let [m (quot n d)]
     (* (quot (* m (+ m 1)) 2) d))) 
 
 (defn solve2
-  "using math"
   [n]
   (let [f (partial mult-sum (- n 1))]
     (+ (f 3) (f 5) (- (f 15)))))
 
-(solve2 1000)
-
+; ------- lein run -m pe.001
 (defn -main [] (println (solve2 1000)))
+
